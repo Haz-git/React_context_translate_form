03ebea670035b210dc2fca66546678170c72ed93
Haz-git <zhouharry1@gmail.com>
2020-09-18 11:04:47 -0700

    Setting up language change

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,29 @@
 import React from 'react';
 
-const App = () => {
-    return (
-        <div>App</div>
-    )
+class App extends React.Component {
+
+    state = {
+        language: 'english'
+    }
+
+    onLanguageChange = language => {
+        this.setState({
+            language
+        });
+    }
+
+    render() {
+        return (
+            <div className='ui containerf'>
+                <div>
+                    Select a language:
+                    <i className='flag us' onClick={() => this.onLanguageChange('english')}/>
+                    <i className='flag nl' onClick={() => this.onLanguageChange('dutch')}/>
+                </div>
+                {this.state.language}
+            </div>
+        )
+    }
 }
 
 
